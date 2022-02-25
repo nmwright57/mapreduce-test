294b2e4e59400f97a36bc8c18b3f8cb94790d7ee
nmwright57 <90349979+nmwright57@users.noreply.github.com>
2022-02-25 13:44:29 -0500

    Update reducer-2.py

--- mapreduce-test-python/top-3/reducer-2.py
+++ mapreduce-test-python/top-3/reducer-2.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
+from operator import itemgetter
+import sys
 
+
+for line in sys.stdin: 
+  line = line.strip().split('\t')
+  hour, ip, count = line
+  hour,ip = line.split('\t')    
+  print '%s\t%s' % ('['+hour+']' +ip, count)
