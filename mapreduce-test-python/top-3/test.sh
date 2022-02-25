9496c0ca4de4d5b4d8a881ca78843c6bcf26cd73
nmwright57 <90349979+nmwright57@users.noreply.github.com>
2022-02-25 13:47:29 -0500

    Update test.sh

--- mapreduce-test-python/top-3/test.sh
+++ mapreduce-test-python/top-3/test.sh
@@ -5,10 +5,22 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /top-3/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /top-3/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/top-3/mapper.py -mapper ../../mapreduce-test-python/top-3/mapper.py \
--file ../../mapreduce-test-python/top-3/reducer.py -reducer ../../mapreduce-test-python/top-3/reducer.py \
+-file ../../mapreduce-test-python/top-3/mapper1.py -mapper ../../mapreduce-test-python/top-3/mapper1.py \
+-file ../../mapreduce-test-python/top-3/reducer1.py -reducer ../../mapreduce-test-python/top-3/reducer1.py \
 -input /top-3/input/* -output /top-3/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /top-3/output/part-00000
+
+
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file ../../mapreduce-test-python/top-3/mapper-2.py -mapper ../../mapreduce-test-python/top-3/mapper-2.py \
+-file ../../mapreduce-test-python/top-3/reducer-2.py -reducer ../../mapreduce-test-python/top-3/reducer-2.py \
+-input /top-3/output/* -output /top-3-2/output/
+
+
+
+
+
+/usr/local/hadoop/bin/hdfs dfs -cat /top-3-2/output/part-00000 | tail -10
 /usr/local/hadoop/bin/hdfs dfs -rm -r /top-3/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /top-3/output/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /top-3-2/output/
 ../../stop.sh
