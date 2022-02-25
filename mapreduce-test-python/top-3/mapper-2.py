7f08665c6c58ca2260c0de9f676448161d41dbf4
nmwright57 <90349979+nmwright57@users.noreply.github.com>
2022-02-25 16:49:22 -0500

    Update mapper-2.py

--- mapreduce-test-python/top-3/mapper-2.py
+++ mapreduce-test-python/top-3/mapper-2.py
@@ -7,18 +7,18 @@ dict_hour_ip_count = {}
 for line in sys.stdin: 
   line = line.strip()
   hour_ip, count = line
-  hour,ip = line.split('\t')
+  hour,ip = line.split(' ')
   
   try:
     hour = int(hour)
     count = int(count)
     dict_hour_ip_count[hour].append([ip,count])
    
- except ValueError:
+  except ValueError:
     pass
     
     
  for hour, ip_count in dict_hour_ip_count.items():
     top3 = sorted(list(ip_count), key = lambda x:x[1], reverse=True)[0:3]
     for ip, count in top3:
-        print '%s\t%s' % ('['+hour+']' +ip, count)
+        print ('%s\t%s' % ('['+hour+']' +ip, count))
